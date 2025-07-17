72383c717d12cdeca4317da2d459fd2b49a234e8
shoney <ddmczp@naver.com>
2025-07-17 17:51:39 +0900

    feat: excel paser 구현

--- parser/json_refine_packing.py
+++ parser/json_refine_packing.py
@@ -19,51 +19,101 @@ def refine_json(input_path, output_path):
         data = json.load(f)
 
     packing_info = []
-    start = False
+    packing_info_start = False
     prev_company = None
+    lab_id = None
+    experiment_info = []
+    experiment_info_start = False
+    prev_code = None
+    special_notes = {}
+    special_notes_start = False
+
     for row in data:
-
-        """
-        1) 포장재 정보 찾는 로직
-        """
-        # 헤더(포장재정보) 찾기: Unnamed: 8에 '포장재업체'가 있으면 그 다음 row부터 시작
-        if not start and str(row.get('Unnamed: 0', '')).strip() == '포장재정보':
-            start = True
+        # 1) 포장재 정보 구간 제어
+        if not packing_info_start and str(row.get('Unnamed: 0', '')).strip() == '포장재정보':
+            packing_info_start = True
             continue
-        if not start:
-            continue
-        # '사양'이 '시료사진'이면 종료
-        if str(row.get('Unnamed: 0', '')).strip() == '처방정보' or str(row.get('Unnamed: 0', '')).strip() == '내용물정보':
-            break
-        # 포장재 정보 row만 추출 (타입/재질/세부사양/포장재업체)
-        type_ = clean_value(row.get('Unnamed: 2'))
-        material = clean_value(row.get('Unnamed: 4'))
-        spec = clean_value(row.get('종류'))
-        company = clean_value(row.get('Unnamed: 8'))
+        if packing_info_start:
+            if str(row.get('Unnamed: 0', '')).strip() in ['처방정보', '내용물정보']:
+                packing_info_start = False 
 
-        # 실제 데이터가 모두 존재할 때만 저장 (타입만 None이 아니면 됨)
-        if type_ is not None:
-            if not company:  # None 또는 빈 문자열 처리
-                company = prev_company
-            else:
-                prev_company = company
+        # 2) 포장재 정보 추출 (start가 True일 때만)
+        if packing_info_start:
+            type_ = clean_value(row.get('Unnamed: 2'))
+            material = clean_value(row.get('Unnamed: 4'))
+            spec = clean_value(row.get('종류'))
+            company = clean_value(row.get('Unnamed: 8'))
+            if type_ is not None:
+                if not company:
+                    company = prev_company
+                else:
+                    prev_company = company
+                packing_info.append({
+                    'type': type_,
+                    'material': material,
+                    'spec': spec,
+                    'company': company
+                })
 
-            packing_info.append({
-                'type': type_,
-                'material': material,
-                'spec': spec,
-                'company': company
-            })
-
-        """
-        2) 처방번호(lab id) 찾는 로직
-        """
+        # 3) lab_id는 반복문 전체에서 항상 체크
         if str(row.get("Unnamed: 2", "")).strip() == "처방번호":
-            lab_id = row.get("종류")
+            lab_id = clean_value(row.get("종류"))
 
+        # 4) 실험정보 추출
+        if not experiment_info_start and (str(row.get('Unnamed: 0', '')).strip() == '시험코드'):
+            experiment_info_start = True
+            continue
+        if experiment_info_start:
+            if str(row.get('Unnamed: 0', '')).strip() in ['처방정보', '내용물정보'] or str(row.get('Unnamed: 0', '')).strip().startswith('※'):
+                experiment_info_start = False
+
+        # 5) 실험정보 추출 (start가 True일 때만)
+        if experiment_info_start:
+            # 각 컬럼에서 값 추출
+            code = clean_value(row.get('Unnamed: 0'))
+            item = clean_value(row.get('Unnamed: 1'))
+            period = clean_value(row.get('Unnamed: 4'))
+            standard = clean_value(row.get('종류'))
+            result = clean_value(row.get('Unnamed: 12')) or clean_value(row.get('립밤(튜브)'))
+            # 시험코드가 None이 아니고, 실제 데이터가 있는 경우만 저장
+
+            if item is not None and item != "-":
+                if code is None:
+                    code = prev_code
+                else:
+                    prev_code = code
+                experiment_info.append({
+                    'code': code,
+                    'item': item,
+                    'period': period,
+                    'standard': standard,
+                    'result': result
+                })
+
+
+        # 6) 특이사항 추출
+        if not special_notes_start and str(row.get('Unnamed: 0', '')).strip() == '시험 특이사항':
+            special_notes_start = True
+            continue
+        if special_notes_start:
+            if str(row.get('Unnamed: 0', '')).strip().startswith('※'):
+                special_notes_start = False
+
+        # 7) 특이사항 추출 (start가 True일 때만)
+        if special_notes_start:
+            special_notes_header = clean_value(row.get('Unnamed: 0'))
+            special_notes_content = clean_value(row.get('Unnamed: 2')) or clean_value(row.get('Unnamed: 1'))
+
+            if special_notes_header is not None and special_notes_header != "-":
+                special_notes[special_notes_header] = special_notes_content
 
     # packing_info 리스트를 'packing_info'라는 상위 키로 감싸서 json으로 저장
-    result = {'packing_info': packing_info, 'lab_id': lab_id}
+    result = {
+        'packing_info': packing_info,
+        'lab_id': lab_id,
+        'experiment_info': experiment_info,
+        'special_notes': special_notes
+        }
     with open(output_path, 'w', encoding='utf-8') as f:
         json.dump(result, f, ensure_ascii=False, indent=2)
 
