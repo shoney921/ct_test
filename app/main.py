3e6f6edbd432229dd8782d0711f9d1d7bbbdf949
shoney <ddmczp@naver.com>
2025-07-23 17:17:40 +0900

    chore: main 실행 방식으로 변경

--- app/main.py
+++ app/main.py
@@ -92,3 +92,7 @@ async def download(request: DownloadRequest):
         fileName="CT20240001_성적서_20250723.xlsx",
         status="ready"
     ) 
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
