95ac93aa0c48603d81c9b8b1e956734de65477fe
shoney <ddmczp@naver.com>
2025-07-28 15:46:41 +0900

    fix: common에서 객체 스키마에서 변수 옵셔널 가능하도록 수정

--- app/schemas/common.py
+++ app/schemas/common.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 
 class PackingInfo(BaseModel):
     type: str
-    material: str
+    material: Optional[str] = None
     spec: Optional[str] = None
     company: Optional[str] = None
 
@@ -23,22 +23,22 @@ class Document(BaseModel):
     document_id: str
     summary: str
     file_name: str
-    test_no: str
-    product_name: str
-    customer: str
-    developer: str
-    requester: str
-    test_count: str
-    test_quantity: str
-    test_date: str
-    expected_date: str
-    writer: str
-    reviewer: str
-    approver: str
+    test_no: Optional[str] = None
+    product_name: Optional[str] = None
+    customer: Optional[str] = None
+    developer: Optional[str] = None
+    requester: Optional[str] = None
+    test_count: Optional[str] = None
+    test_quantity: Optional[str] = None
+    test_date: Optional[str] = None
+    expected_date: Optional[str] = None
+    writer: Optional[str] = None
+    reviewer: Optional[str] = None
+    approver: Optional[str] = None
     packing_info: List[PackingInfo]
-    lab_id: str
-    lab_info: str
-    optimum_capacity : str
+    lab_id: Optional[str] = None
+    lab_info: Optional[str] = None
+    optimum_capacity : Optional[str] = None
     experiment_info: List[ExperimentInfo]
     special_notes: List[SpecialNote]
     download_url: str
